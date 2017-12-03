478916ccab1fe92b119164d9ad1cf860a158ec38
trotsenkopolina <34116642+trotsenkopolina@users.noreply.github.com>
2017-12-02 20:57:41 -0500

    main 1st trial code

--- Main.c
+++ Main.c
@@ -5,9 +5,20 @@
  *      Author: nicolas
  */
 #include <stdio.h>
+#include <stdlib.b>
+#include <unistd.h>
 
 int main(void)
 {
+  //intitialize TURN shell variable to 0 
+  //producer has TURN = 0; consumer has TURN = 1
+  setenv("TURN","0",1);
+  
+  //launch the clones
+  int pid = fork();
+  if (pid == -1)exit(1);
+  if (pid == 0) {producer();}
+  if (pid != 0) {consumer();}
 
 
 }
